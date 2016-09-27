86b07ce8de89b39caaeb31b6141e854f243af8d1
'fanlychie' <'fanlychie@yeah.net'>
2016-09-27 16:18:17 +0800

    将回车换行符改成换行符

--- src/main/java/org/fanlychie/util/FileUtils.java
+++ src/main/java/org/fanlychie/util/FileUtils.java
@@ -275,7 +275,7 @@ public final class FileUtils {
 		 */
 		public String read() {
 			StringBuilder builder = new StringBuilder();
-			readlines((line) -> builder.append(line).append("\r\n"));
+			readlines((line) -> builder.append(line).append("\n"));
 			return builder.length() > 0 ? builder.toString().substring(0, builder.length() - 2) : "";
 		}
 		
